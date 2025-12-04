cc6764f83564eb9c0212d391460159e72b858b74
Hans Gumiran <hansjessieaugusto.gumiran@cvsu.edu.ph>
2025-12-05 00:22:10 +0800

    Create Television.java

---
+++ Television.java
@@ -0,0 +1,29 @@
+public class Television extends Device {
+    private int channel;
+    private int volume;
+
+    public Television() {
+        this.channel = 1;
+        this.volume = 10;
+    }
+
+    public void setChannel(int channel) {
+        this.channel = channel;
+        System.out.println("TV channel set to " + channel);
+        showStatus();
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+        System.out.println("TV volume set to " + volume + "%");
+        showStatus();
+    }
+
+    @Override
+    public void showStatus() {
+        if (isOn())
+            System.out.println("TV Status → Channel: " + channel + ", Volume: " + volume + "%");
+        else
+            System.out.println("TV is OFF.");
+    }
+}
